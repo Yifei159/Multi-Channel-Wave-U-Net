0c06eee5fc2c3197bbfba1fc54d6df083fd8dc76
Yifei Wei <yifeiwei7@outlook.com>
2025-09-11 21:03:21 +1000

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -20,8 +20,7 @@ def crop_and_concat(x1, x2, match_feature_dim=True):
     return tf.concat([x1, x2], axis=2)
 
 
-# ---------------------------------------------------------------------- #
-#                           DATA  AUGMENTATION                           #
+# DATA  AUGMENTATION
 def random_amplify(sample):
 
     for key, val in sample.items():
